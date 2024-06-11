d9cea0dc4fe87fc25ab473ab99a02628c5411b5f
advikak <advika.kumari29@gmail.com>
2024-06-11 13:28:37 -0700

    Sidebar and Icon Changes

--- src/app/ui/home/sidebar.tsx
+++ src/app/ui/home/sidebar.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image'; // if you are using images for logos or icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faSearch, faCog } from '@fortawesome/free-solid-svg-icons';
+import { faBorderAll, faSearch, faCog } from '@fortawesome/free-solid-svg-icons';
 
 export default function SideBar() {
   return (
@@ -19,25 +19,36 @@ export default function SideBar() {
           {/* Adjust the padding-left to increase space between icon and text */}
           {/* Adjust the width or padding-right to shift overall content to the right */}
           <Link href="/home" className="group flex items-center pl-10 pr-4 h-12 w-full md:w-60 hover:bg-gray-300">
-              <FontAwesomeIcon icon={faHouse} className="w-5 h-5 mr-4" /> {/* Add margin-right to create space between icon and text */}
-              <span className="text-sm font-medium text-gray-700">Home</span>
+              <Image src="/Dashboard.png" alt="Logo" width={50} height={50} className="mr-3" />{/* Add margin-right to create space between icon and text */}
+              <span className="text-sm font-medium text-gray-700">Dashboard</span>
           </Link>
           <Link href="/home/search" className="group flex items-center pl-10 pr-4 h-12 w-full md:w-60 hover:bg-gray-300">
-              <FontAwesomeIcon icon={faSearch} className="w-5 h-5 mr-4" />
+              <Image src="/Search.png" alt="Logo" width={50} height={50} className="mr-3" />
               <span className="text-sm font-medium text-gray-700">Search</span>
           </Link>
           <Link href="/home/manage" className="group flex items-center pl-10 pr-4 h-12 w-full md:w-60 hover:bg-gray-300">
-              <FontAwesomeIcon icon={faCog} className="w-5 h-5 mr-4" />
+              <Image src="/Manage.png" alt="Logo" width={50} height={50} className="mr-3" />
               <span className="text-sm font-medium text-gray-700">Manage</span>
           </Link>
+          <Link href="/home/manage" className="group flex items-center pl-10 pr-4 h-12 w-full md:w-60 hover:bg-gray-300">
+              <Image src="/Message.png" alt="Logo" width={50} height={50} className="mr-3" />
+              <span className="text-sm font-medium text-gray-700">Message</span>
+          </Link>
         </nav>
+        <div id='morebutton' className="flex justify-center mt-auto mb-10">
+          <Link href="/home/more" className="group flex items-center pl-10 pr-4 h-12 w-full md:w-60 hover:bg-gray-300">
+              <Image src="/ThreeBars.png" alt="Logo" width={50} height={50} className="mr-3" />
+              <span className="text-sm font-medium">More</span> 
+          </Link>
+        </div>
+        
 
         {/* Sign Out Button */}
-        <div id='logoutbutton' className="flex justify-center mt-auto mb-10">
+{/*         <div id='logoutbutton' className="flex justify-center mt-auto mb-10">
           <Link href="/" className="flex items-center justify-center h-12 w-4/5 rounded-lg bg-blue-500 text-white transition-colors hover:bg-blue-400">
               <span className="text-sm font-medium">Log out</span> 
           </Link>
-        </div>
+        </div> */}
       </div>
     </div>
   );
