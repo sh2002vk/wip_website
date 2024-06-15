e3d5f862397df5a594779f6964945251afbe9b7a
advikak <advika.kumari29@gmail.com>
2024-06-15 15:09:55 -0700

    Sticky header dashboard

--- src/app/ui/dashboard/applications.tsx
+++ src/app/ui/dashboard/applications.tsx
@@ -21,34 +21,34 @@ const applications = () => {
       time: '2h'
     },
     {
-        name: 'Melissa',
-        university: 'University of Waterloo',
-        role: 'Data Scientist',
-        time: '2h'
-      }
+      name: 'Gloria',
+      university: 'Hardward University',
+      role: 'Data Scientist',
+      time: '2h'
+    }
   ];
 
   return (
     <div className="h-full overflow-y-auto">
-      <div className="p-4">
-        <h1 className="text-xl font-semibold">New Applications <span className="bg-gray-200 text-sm font-medium py-1 px-2 rounded-full ml-2">8</span></h1>
-        <div className="mt-4">
-          {data.map((item, index) => (
-            <div key={index} className="flex items-center justify-between py-3 border-b">
-              <div className="flex items-center">
-                <div className="bg-gray-300 rounded-full h-10 w-10 flex items-center justify-center text-lg font-bold text-gray-500">{item.name.charAt(0)}</div>
-                <div className="ml-4">
-                  <p className="text-md font-medium">{item.name}</p>
-                  <p className="text-sm text-gray-500">{item.university}</p>
-                </div>
-              </div>
-              <div className="flex items-center">
-                <span className="bg-gray-200 text-sm font-light py-1 px-2 rounded-full">{item.role}</span>
-                <span className="ml-4 text-gray-400 text-sm">{item.time}</span>
+      <div className="p-4 sticky top-0 bg-[#F5f5f5] z-10">
+        <h1 className="text-xl font-semibold ">New Applications <span className="bg-gray-200 text-sm font-medium py-1 px-2 rounded-full ml-2">8</span></h1>
+      </div>
+      <div className="p-4 pt-0">
+        {data.map((item, index) => (
+          <div key={index} className="flex items-center justify-between py-3 border-b">
+            <div className="flex items-center">
+              <div className="bg-gray-300 rounded-full h-10 w-10 flex items-center justify-center text-lg font-bold text-gray-500">{item.name.charAt(0)}</div>
+              <div className="ml-4">
+                <p className="text-md font-medium">{item.name}</p>
+                <p className="text-sm text-gray-500">{item.university}</p>
               </div>
             </div>
-          ))}
-        </div>
+            <div className="flex items-center">
+              <span className="bg-gray-200 text-sm font-light py-1 px-2 rounded-full">{item.role}</span>
+              <span className="ml-4 text-gray-400 text-sm">{item.time}</span>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
