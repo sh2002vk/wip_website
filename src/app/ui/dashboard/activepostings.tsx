0a9c841fb5843448ce66539f64d149f6d7ea1ab9
= <=>
2024-06-16 16:21:41 -0700

    new branch and activePostings

---
+++ src/app/ui/dashboard/activepostings.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const ActivePostings = () => {
+    return (
+        <div className="h-full overflow-y-auto">
+            <p>Testing Active Postings</p>
+        </div>
+    )
+};
+
+export default ActivePostings;
