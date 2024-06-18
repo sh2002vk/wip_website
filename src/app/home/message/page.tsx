ef0b1f83df82f060ec520385deb79621690c31eb
advikak <advika.kumari29@gmail.com>
2024-06-18 10:12:33 -0700

    Added message component

---
+++ src/app/home/message/page.tsx
@@ -0,0 +1,10 @@
+import MessageLayout from './layout'
+// import Bookmarks from '@/app/ui/manage/bookmarks';
+
+export default function MessagePage() {
+  return (
+    <MessageLayout title="MESSAGE">
+      <p>Messages</p>
+    </MessageLayout>
+  );
+}
