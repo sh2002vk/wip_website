5a2d2286b84ab5ecb1ca3afe023f6ba8d9733d6b
Leyang Pan <65788181+YuanSu526@users.noreply.github.com>
2024-06-19 20:18:36 -0700

    able to edit text box after clicking faPencil

--- src/app/home/manage/layout.tsx
+++ src/app/home/manage/layout.tsx
@@ -1,5 +1,5 @@
-'use client';
-import React, { useState } from 'react';
+'use client'
+import React, { useState, useEffect } from 'react';
 import Bookmarks from '@/app/ui/manage/recruiters/bookmarks';
 import JobDetails from '@/app/ui/manage/recruiters/jobDetails';
 import SideBar from "@/app/ui/home/sidebar"; 
@@ -10,7 +10,80 @@ type LayoutProps = {
 };
 
 const ManageLayout = ({ children, title }: LayoutProps) => {
+
+  const initialDrafts = [
+    {
+      id: '1',
+      company: "WorkInProgress",
+      title: "Software Engineer",
+      type: "Full-time",
+      location: "Vancouver, BC",
+      jobDetail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      jobQualification: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      jobDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      draft: true,
+    },
+    {
+      id: '2',
+      company: "WorkInProgress",
+      title: "To Delete",
+      type: "Full-time",
+      location: "",
+      jobDetail: "",
+      jobBenefits: "",
+      jobDescription: "",
+      draft: true,
+    },
+    {
+      id: '3',
+      company: "WorkInProgress",
+      title: "Test Index 1",
+      type: "Full-time",
+      location: "",
+      jobDetail: "",
+      jobBenefits: "",
+      jobDescription: "",
+      draft: true,
+    },
+    {
+      id: '4',
+      company: "WorkInProgress",
+      title: "Test Index 2",
+      type: "Full-time",
+      location: "",
+      jobDetail: "",
+      jobBenefits: "",
+      jobDescription: "",
+      draft: true,
+    },
+    {
+      id: '5',
+      company: "WorkInProgress",
+      title: "Test Index 3",
+      type: "Full-time",
+      location: "",
+      jobDetail: "",
+      jobBenefits: "",
+      jobDescription: "",
+      draft: true,
+    },
+  ]
+
+  const initialCompleted = [
+    {
+      id: '6',
+      company: "WorkInProgress",
+      title: "Software Engineer",
+      type: "Full-time",
+      jobDetail: "Currently seeking an experienced software Developer for development of our next generation security solution impacting multiple products...",
+      jobBenefits: "Has listed the base salary ranges it in good faith expects to pay applicants for this role in the locations listed, as of the time of this posting.",
+      jobDescription: "You will do full stack development working on both game client and backend services.",
+      draft: false,
+    },
+  ]
+
   const [selectedJob, setSelectedJob] = useState(null);
+  const [jobs, setJobs] = useState(initialDrafts);
 
   const handleSelectJob = (job) => {
     setSelectedJob(job);
@@ -20,14 +93,38 @@ const ManageLayout = ({ children, title }: LayoutProps) => {
     setSelectedJob(null);
   };
 
+  const handleJobUpdate = (updatedJob) => {
+    console.log("Updated Job:", updatedJob);
+    setJobs((currentJobs) => {
+      const updatedJobs = currentJobs.map((job) => 
+        job.id === updatedJob.id ? updatedJob : job
+      );
+      console.log("Updated Jobs List:", updatedJobs);
+      return updatedJobs;
+    });
+  };
+
+  useEffect(() => {
+    console.log("Jobs State:", jobs);
+  }, [jobs]);
+
   return (
     <div className="flex h-screen">
       <div className="w-full md:w-64 flex-none h-screen overflow-auto">
-        <Bookmarks onSelectJob={handleSelectJob} />
+        <Bookmarks 
+          onSelectJob={handleSelectJob} 
+          initialDrafts={jobs} 
+          initialCompleted={initialCompleted}
+        />
       </div>
       <div className="flex-1 p-4 overflow-x-auto overflow-y-auto no-scrollbar">
         {selectedJob && (
-          <JobDetails job={selectedJob} onClose={handleCloseJobDetails} />
+          <JobDetails 
+            key={selectedJob.id} 
+            job={selectedJob} 
+            onClose={handleCloseJobDetails} 
+            onJobUpdate={handleJobUpdate} // Pass the update handler
+          />
         )}
       </div>
     </div>
--- src/app/ui/manage/recruiters/jobDetails.tsx
+++ src/app/ui/manage/recruiters/jobDetails.tsx
@@ -1,15 +1,14 @@
-// JobDetails.tsx
-'use client';
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; //used for the close button on each job posting
+'use client'
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
 import { faPencilAlt, faTimes, faSearch, faCheck } from '@fortawesome/free-solid-svg-icons';
 import CollapsibleCard from './cards/collapsableCard';
 import JobOptionToggle from './cards/jobOptionToggle';
 import "./style.css"
 
-
 interface JobDetailsProps {
   job: {
+    id: string;
     company: string;
     title: string;
     location: string;
@@ -19,76 +18,111 @@ interface JobDetailsProps {
     jobBenefits: string;
     type: string;
   };
-
   onClose: () => void;
+  onJobUpdate: (updatedJob: any) => void; // Function to update job details
 }
 
-const JobDetails: React.FC<JobDetailsProps> = ({ job, onClose }) => {
+const JobDetails: React.FC<JobDetailsProps> = ({job, onClose, onJobUpdate }) => {
+  const [isJobDetailsOpen, setIsJobDetailsOpen] = useState(false);
+  const [isJobDescriptionOpen, setIsJobDescriptionOpen] = useState(false);
+  const [isJobQualificationOpen, setIsJobQualificationOpen] = useState(false);
+  const [isPencilClicked, setIsPencilClicked] = useState(false);
 
-    const [isJobDetailsOpen, setIsJobDetailsOpen] = useState(false);
-    const [isJobDescriptionOpen, setIsJobDescriptionOpen] = useState(false);
-    const [isJobQualificationOpen, setIsJobQualificationOpen] = useState(false);
+  const [jobDetailsContent, setJobDetailsContent] = useState(job.jobDetail);
+  const [jobDescriptionContent, setJobDescriptionContent] = useState(job.jobDescription);
+  const [jobQualificationContent, setJobQualificationContent] = useState(job.jobQualification);
+
+  useEffect(() => {
+    setJobDetailsContent(job.jobDetail);
+    setJobDescriptionContent(job.jobDescription);
+    setJobQualificationContent(job.jobQualification);
+  }, [job]);
+
+  const toggleJobDetails = () => setIsJobDetailsOpen(!isJobDetailsOpen);
+  const toggleJobDescription = () => setIsJobDescriptionOpen(!isJobDescriptionOpen);
+  const toggleJobQualification = () => setIsJobQualificationOpen(!isJobQualificationOpen);
   
-    const toggleJobDetails = () => setIsJobDetailsOpen(!isJobDetailsOpen);
-    const toggleJobDescription = () => setIsJobDescriptionOpen(!isJobDescriptionOpen);
-    const toggleJobQualification = () => setIsJobQualificationOpen(!isJobQualificationOpen);
+  const togglePencil = () => {
+    if (isPencilClicked) {
+        console.log("Updating");
+        onJobUpdate({
+            ...job,
+            jobDetail: jobDetailsContent,
+            jobDescription: jobDescriptionContent,
+            jobQualification: jobQualificationContent,
+        });
+    }
+    setIsPencilClicked(!isPencilClicked);
+  };
 
-
-    return (
-        <div className="p-4 bg-white overflow-y-auto no-scrollbar">
-            <div className="border-b border-gray-300 mb-4">
-                <div className="flex justify-end">
-                    {/*TODO: On click handler*/}
-                    <button onClick={onClose} className="bg-transparent font-bold text-black hover:text-gray-600">
-                        <FontAwesomeIcon icon={faTimes} size="2x"/>
-                    </button>
-                </div>       
-                <div className="flex justify-between">
-                    <h2 className="text-xl font-bold mb-2">{job.title}</h2>
-                    <div className="flex space-x-4">
-                        <FontAwesomeIcon icon={faCheck} size="lg" />
-                        <FontAwesomeIcon icon={faPencilAlt} size="lg" />
-                        <FontAwesomeIcon icon={faSearch} size="lg" />
-                    </div>
-                </div>
-                <div className="mt-2 mb-4">
-                    <p className="text-lg font-bold">{job.company}</p>
-                    <p className="text-lg">{job.location}</p>
-                </div>
-            </div>
-
-            <div className="border-b border-gray-300 mb-4">
-                <CollapsibleCard title="Job Details" content={job.jobDetail} />
-                <CollapsibleCard title="Job Description" content={job.jobDescription} />
-                <CollapsibleCard title="Job Qualification" content={job.jobQualification} />
-            </div>
-
-            <div className="">
-                <h2 className="text-xl text-orange-500 font-bold mb-2">Optional Components</h2>
-                <div className="flex justify-between w-full">
-                    <h2 className="text-lg font-bold">Cover Letter</h2>
-                    <JobOptionToggle />
-                </div>
-                <div className="flex justify-between w-full">
-                    <h2 className="text-lg font-bold">Video Application</h2>
-                    <JobOptionToggle />
-                </div>
-                <div className="flex justify-between w-full">
-                    <h2 className="text-lg font-bold">Cognitive Test</h2>
-                    <JobOptionToggle />
-                </div>
-                <div className="flex justify-between w-full">
-                    <h2 className="text-lg font-bold">English Sample</h2>
-                    <JobOptionToggle />
-                </div>
-                <div className="flex justify-between w-full">
-                    <h2 className="text-lg font-bold">Online Assessment</h2>
-                    <JobOptionToggle />
-                </div>
-            </div>
-            
+  return (
+    <div key={job.id} className="p-4 bg-white overflow-y-auto no-scrollbar">
+      <div className="border-b border-gray-300 mb-4">
+        <div className="flex justify-end">
+          <button onClick={onClose} className="bg-transparent font-bold text-black hover:text-gray-600">
+            <FontAwesomeIcon icon={faTimes} size="2x"/>
+          </button>
+        </div>       
+        <div className="flex justify-between">
+          <h2 className="text-xl font-bold mb-2">{job.title}</h2>
+          <div className="flex space-x-4">
+            <FontAwesomeIcon icon={faCheck} size="lg" />
+            <FontAwesomeIcon icon={faPencilAlt} size="lg" onClick={togglePencil} />
+            <FontAwesomeIcon icon={faSearch} size="lg" />
+          </div>
         </div>
-    );
+        <div className="mt-2 mb-4">
+          <p className="text-lg font-bold">{job.company}</p>
+          <p className="text-lg">{job.location}</p>
+        </div>
+      </div>
+
+      <div className="border-b border-gray-300 mb-4">
+        <CollapsibleCard 
+          title="Job Details"
+          content={jobDetailsContent}
+          editable={isPencilClicked}
+          onContentChange={setJobDetailsContent}
+        />
+        <CollapsibleCard 
+          title="Job Description"
+          content={jobDescriptionContent}
+          editable={isPencilClicked}
+          onContentChange={setJobDescriptionContent}
+        />
+        <CollapsibleCard 
+          title="Job Qualifications"
+          content={jobQualificationContent}
+          editable={isPencilClicked}
+          onContentChange={setJobQualificationContent}
+        />
+      </div>
+
+      <div className="">
+        <h2 className="text-xl text-orange-500 font-bold mb-2">Optional Components</h2>
+        <div className="flex justify-between w-full">
+          <h2 className="text-lg font-bold">Cover Letter</h2>
+          <JobOptionToggle />
+        </div>
+        <div className="flex justify-between w-full">
+          <h2 className="text-lg font-bold">Video Application</h2>
+          <JobOptionToggle />
+        </div>
+        <div className="flex justify-between w-full">
+          <h2 className="text-lg font-bold">Cognitive Test</h2>
+          <JobOptionToggle />
+        </div>
+        <div className="flex justify-between w-full">
+          <h2 className="text-lg font-bold">English Sample</h2>
+          <JobOptionToggle />
+        </div>
+        <div className="flex justify-between w-full">
+          <h2 className="text-lg font-bold">Online Assessment</h2>
+          <JobOptionToggle />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default JobDetails;
