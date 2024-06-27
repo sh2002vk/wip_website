fdc5f24ae0b8e60e8a0086fe8d28a2d7df3f3345
= <=>
2024-06-27 15:57:34 -0600

    Added states and dynamic text to job option toggles

--- src/app/ui/manage/recruiters/cards/jobOptionToggle.tsx
+++ src/app/ui/manage/recruiters/cards/jobOptionToggle.tsx
@@ -1,20 +1,24 @@
 import React, { useState } from 'react';
 
-const JobOptionToggle: React.FC = () => {
+interface JobOptionToggleProps {
+  isSelected: boolean;
+  onToggle: () => void;
+}
+const JobOptionToggle: React.FC<JobOptionToggleProps> = ({isSelected, onToggle}) => {
   const [isToggled, setIsToggled] = useState(false);
 
-  const handleToggle = () => setIsToggled(!isToggled);
+  // const handleToggle = () => setIsToggled(!isToggled);
 
   return (
     <div
       className={`w-12 h-6 flex items-center bg-gray-300 rounded-full p-1 cursor-pointer ${
-        isToggled ? 'bg-orange-400' : 'bg-gray-300'
+        isSelected ? 'bg-orange-400' : 'bg-gray-300'
       }`}
-      onClick={handleToggle}
+      onClick={onToggle}
     >
       <div
         className={`bg-white w-5 h-5 rounded-full shadow-md transform duration-300 ease-in-out ${
-          isToggled ? 'translate-x-5' : ''
+          isSelected ? 'translate-x-5' : ''
         }`}
       ></div>
     </div>
