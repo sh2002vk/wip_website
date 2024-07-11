76445a7ff7597f1d9a71f8b02fbdd4b0bd1ac71b
= <=>
2024-07-11 01:01:00 -0700

    added manage functionality

--- src/app/ui/manage/recruiters/jobDashboardTables.tsx
+++ src/app/ui/manage/recruiters/jobDashboardTables.tsx
@@ -1,14 +1,35 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 import StudentProfileView from "@/app/ui/search/studentProfileView";
+
+interface Application {
+    Status: string;
+    StudentID: number;
+}
+
+interface Shortlist {
+    StudentID: number;
+}
+
+interface Student {
+    StudentID: number;
+    FirstName: string;
+    LastName: string;
+    School: string;
+    Email: string;
+    AcademicMajor: string;
+    GPA: number;
+}
+
 interface jobDashboardTableProps {
     isActionNeeded?: boolean,
     isInvited?: boolean,
     isContacting?: boolean,
-    onCardClick: (student: any, isApplication: boolean) => void
+    onCardClick: (student: any, isApplication: boolean) => void,
+    data: Shortlist[] | Application[];
 }
 
-const JobDashboardTables: React.FC<jobDashboardTableProps> = ({isActionNeeded, isInvited, isContacting, onCardClick}) => {
+const JobDashboardTables: React.FC<jobDashboardTableProps> = ({data, isActionNeeded, isInvited, isContacting, onCardClick}) => {
     const applicantData = [
         {
             StudentID: 1,
@@ -105,20 +126,20 @@ const JobDashboardTables: React.FC<jobDashboardTableProps> = ({isActionNeeded, i
         }
     ];
 
-    const contactingStudents = applicantData.filter(applicant =>
-        shortlistData.some(shortlist => shortlist.StudentID === applicant.StudentID)
-    ); //Contacting list should be the && of Job shortlist and job applicant studentIDs
+    // const contactingStudents = applicantData.filter(applicant =>
+    //     shortlistData.some(shortlist => shortlist.StudentID === applicant.StudentID)
+    // ); //Contacting list should be the && of Job shortlist and job applicant studentIDs
+    //
+    // let dataToShow: any[] = [];
+    // if (isActionNeeded) {
+    //     dataToShow = applicantData;
+    // } else if (isInvited) {
+    //     dataToShow = shortlistData;
+    // } else if (isContacting) {
+    //     dataToShow = contactingStudents;
+    // }
 
-    let dataToShow: any[] = [];
-    if (isActionNeeded) {
-        dataToShow = applicantData;
-    } else if (isInvited) {
-        dataToShow = shortlistData;
-    } else if (isContacting) {
-        dataToShow = contactingStudents;
-    }
-
-    const sortedData = applicantData.sort((a, b) => {
+    const sortedData = (data as Application[]).sort((a, b) => {
         if (a.Status === 'APPLIED' && b.Status !== 'APPLIED') {
             return -1;
         }
@@ -128,23 +149,49 @@ const JobDashboardTables: React.FC<jobDashboardTableProps> = ({isActionNeeded, i
         return 0;
     });
 
+    const [tableItems, setTableItems] = useState([]);
+    useEffect(() => {
+        const getTileInformation = async () => {
+            try {
+                const items: (Student & { Status?: string, ApplicationTime?: Date , ApplicationID? : number })[] = [];
+                for (const a of data) {
+                    const response = await fetch(`http://localhost:4000/profile/student/getFullProfile?studentID=${a.StudentID}`);
+                    const profile = await response.json();
+                    items.push({
+                        ...profile.data,
+                        Status: 'Status' in a ? a.Status : undefined, // Use the status if available
+                        ApplicationTime: 'ApplicationTime' in a ? a.ApplicationTime : undefined,
+                        ApplicationID: 'ApplicationID' in a ? a.ApplicationID : undefined
+                    });
+                }
+                setTableItems(items);
+            } catch (error) {
+                console.error('Error fetching student profiles:', error);
+            }
+        };
+
+        if (data.length > 0) {
+            getTileInformation();
+        }
+    }, [data]);
+
     return (
         <div className="h-full overflow-y-auto flex flex-col relative">
                 <div className="p-4 sticky top-0 bg-[#F5f5f5] z-10 border-b">
                     {isActionNeeded ? (
-                        <h1 className="text-xl font-light ">Action Needed <span className="bg-gray-200 text-sm font-medium py-1 px-2 rounded-full ml-2">{sortedData.length}</span></h1>
+                        <h1 className="text-xl font-light ">Action Needed <span className="bg-gray-200 text-sm font-medium py-1 px-2 rounded-full ml-2">{tableItems.length}</span></h1>
                     ) : (
                         <>
                             {isInvited ? (
-                                <h1 className="text-xl font-light ">Invited <span className="bg-gray-200 text-sm font-medium py-1 px-2 rounded-full ml-2">{shortlistData.length}</span></h1>
+                                <h1 className="text-xl font-light ">Invited <span className="bg-gray-200 text-sm font-medium py-1 px-2 rounded-full ml-2">{tableItems.length}</span></h1>
                             ) : (
-                                <h1 className="text-xl font-light ">Contacting <span className="bg-gray-200 text-sm font-medium py-1 px-2 rounded-full ml-2">{contactingStudents.length}</span></h1>
+                                <h1 className="text-xl font-light ">Contacting <span className="bg-gray-200 text-sm font-medium py-1 px-2 rounded-full ml-2">{tableItems.length}</span></h1>
                             )}
                         </>
                     )}
                 </div>
                 <div className="p-4 pt-0">
-                    {dataToShow.map((item, index) => (
+                    {tableItems.map((item: Student, index) => (
                         <div key={index} className="flex justify-between py-3 border-b"
                              onClick={() => onCardClick(item, !isInvited)} // !isInvited tells the parent component that this is not an application
                         >
